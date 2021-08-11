2bd054870cba8cb3b085f3106e69b243b4c7dfd9
Matthew Kupinski <mkupinski@optics.arizona.edu>
2021-08-11 10:11:28 -0700

    asdf

--- Sources/RKView/RKView.swift
+++ Sources/RKView/RKView.swift
@@ -109,7 +109,7 @@ public class RKView : ARView {
     
     public override func scrollWheel(with event: NSEvent) {
         var deltaX = dragFactor * event.deltaX * cos(theta) * cos(phi)
-        deltaX += dragFactor * event.deltaY * sin(theta) * sin(phi)
+        deltaX -= dragFactor * event.deltaY * sin(theta) * sin(phi)
         let deltaY = event.deltaY * dragFactor * cos(phi)
         var deltaZ = dragFactor * event.deltaX * sin(theta) * cos(phi)
         deltaZ += dragFactor * event.deltaY * cos(theta) * sin(phi)
